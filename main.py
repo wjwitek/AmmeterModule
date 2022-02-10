2b1201c8da76bb70b94ce5fc48e250cccc8e7964
wjwitek <weronikaj.witek@gmail.com>
2022-02-10 11:30:52 +0100

    first version

---
+++ main.py
@@ -0,0 +1,58 @@
+import PySimpleGUI as sg
+import time
+import sys
+import argparse
+from pymavlink import mavutil
+from utils import recalculate_current
+from random import random
+
+# read command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=str, help="enter virtual port, eg. COM14",
+                    nargs='?', default='COM14', const=0)
+port = parser.parse_args().port
+
+# establish connection via mavlink
+# master = mavutil.mavlink_connection(port)
+# master.wait_heartbeat()
+# master.mav.param_request_list_send(master.target_system, master.target_component)
+
+# create window layout
+sg.ChangeLookAndFeel('SandyBeach')
+sg.SetOptions(element_padding=(0, 0))
+
+layout = [[sg.Text('')],
+          [sg.Text('Power Monitor', font=('Times New Roman', 30, 'bold'))],
+          [sg.Text(font=('Times New Roman', 30), key='voltage')],
+          [sg.Text(font=('Times New Roman', 30), key='current')],
+          [sg.Exit(button_color=('black', 'firebrick4'), key='Exit')]]
+
+window = sg.Window('Battery Monitor', layout, no_titlebar=True, auto_size_buttons=False, keep_on_top=True,
+                   grab_anywhere=True, element_justification='c')
+
+# set default values
+voltage = 0
+raw_current = 0
+current = 0
+
+# main loop
+while True:
+    time.sleep(0.1)
+    # try reading value via mavlink
+    try:
+        # message = master.recv_match(type='SYS_STATUS', blocking=True).to_dict()
+        # voltage = message['voltage_battery'] / 1000
+        # raw_current = message['current_battery'] / 100
+        voltage = random()
+        raw_current = random()
+        current = recalculate_current(raw_current)
+    except Exception as error:
+        print(error)
+        sys.exit(0)
+    # update window
+    event, values = window.read(timeout=10)
+    window['voltage'].update("Voltage: {:.4f} V".format(round(voltage, 4)))
+    window['current'].update("Current: {:.4f} A".format(round(current, 4)))
+
+    if event == 'Exit':
+        break
---
+++ utils.py
@@ -0,0 +1,2 @@
+def recalculate_current(x, a3=0.0028, a2=-0.0769, a1=1.4002, a0=1.4296):
+    return a3*x**3+a2*x**2+a1*x+a0
